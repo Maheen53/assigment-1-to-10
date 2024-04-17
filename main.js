1df2330370fb6bdde4db07dee3b57f8c5ce2b1cc
“[firstname <maheenimtiaz536@gmail.com>
2024-04-17 16:04:57 -0700

    i complete 1 till 10 assignment

---
+++ main.js
@@ -0,0 +1,67 @@
+"use strict";
+// TASK #1
+// let personname : string = "Eric"
+Object.defineProperty(exports, "__esModule", { value: true });
+// let message : string = "would you like to learn some python today"
+// console.log(`hello ${personname} ${message}`);
+// // task # 2
+// let personName : string = "Maheen Imtiaz";
+// console.log("lowercase:" , personName.toLowerCase());
+// // uppercase
+// console.log("uppercase:",personName.toLocaleUpperCase());
+// // TITLECASE
+// console.log("titlecase:",personName.replace(/\bw/g,c => c.toUpperCase()));
+// //  tasl#4
+// let authoreName : string = "Albert Einstein"
+// let quote : string ="A person who never made a mistake never tried anything new";
+// console.log(`${authoreName}once said ${quote}`);
+// // task # 5
+// let famouspersonName : string = "Albert Einstine"
+// let message : string = "A person who never made a mistake never tried anything new";
+// console.log(`${famouspersonName}once said ${message} `);
+// task# 6
+// let personName : string = `\n\t Maheen Imtiaz\t\n`;
+// console.log(personName);
+// let stripped : string = personName.trim();
+// console.log(stripped);
+// task 7 and 8
+// console.log(5+3);
+// console.log(10-2);
+// console.log(2*4);
+// console.log(16/2);
+//  task # 9
+// let favoriteNumber : number =  2 ;
+// console.log(`${favoriteNumber} is  my lucky number  `)
+// task # 10 
+my;
+name;
+is;
+maheen;
+4..;
+4..;
+2024;
+console.log(5 * 3);
+this;
+program;
+will;
+multiplication;
+console.log("hello world");
+this;
+program;
+will;
+simple;
+text;
+hello;
+world;
+//    // task # 11
+// let myFriends : string [] = ["shibra","meera","uzma"]
+// console.log(myFriends[0])
+// console.log(myFriends[1])
+// console.log(myFriends[2])
+// // task 12
+// let names : string [] = ["shibra","meera","uzma"]
+// let message : string = "what are you doing";
+// console.log(names[0] + message)
+// console.log(names[1] + message)
+// console.log(names[2] + message)
+//   // task # 13
